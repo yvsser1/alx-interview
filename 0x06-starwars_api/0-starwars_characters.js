93a12bbb5fd4865f8a5e008eeff7e169a56ead07
Yasser <yasserdalouzi@gmail.com>
2025-01-07 12:17:02 +0100

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,31 @@
 #!/usr/bin/node
 
 const request = require('request');
+
 const movieId = process.argv[2];
-const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+const movieEndpoint = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
 
-// Get the movie data first
-request(filmUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
+function sendRequest (characterList, index) {
+  if (characterList.length === index) {
     return;
   }
 
-  if (response.statusCode !== 200) {
-    console.error('Invalid status code:', response.statusCode);
-    return;
-  }
-
-  const movieData = JSON.parse(body);
-  const characters = movieData.characters;
-
-  // Function to get character name from URL
-  const getCharacterName = (characterUrl) => {
-    return new Promise((resolve, reject) => {
-      request(characterUrl, (error, response, body) => {
-        if (error) {
-          reject(error);
-          return;
-        }
-        if (response.statusCode !== 200) {
-          reject(new Error(`Invalid status code: ${response.statusCode}`));
-          return;
-        }
-        const characterData = JSON.parse(body);
-        resolve(characterData.name);
-      });
-    });
-  };
-
-  // Process characters in sequence to maintain order
-  const printCharactersInOrder = async () => {
-    try {
-      for (const characterUrl of characters) {
-        const name = await getCharacterName(characterUrl);
-        console.log(name);
-      }
-    } catch (error) {
-      console.error('Error getting character:', error);
+  request(characterList[index], (error, response, body) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(JSON.parse(body).name);
+      sendRequest(characterList, index + 1);
     }
-  };
+  });
+}
 
-  printCharactersInOrder();
+request(movieEndpoint, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const characterList = JSON.parse(body).characters;
+
+    sendRequest(characterList, 0);
+  }
 });
